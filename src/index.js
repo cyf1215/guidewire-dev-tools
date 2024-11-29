ace34ac8471f2b5eb1c8e26201c2b9ba620bf45d
Ifan Cao <ifan.cao@capgemini.com>
2024-11-29 18:24:32 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,76 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
+const started = require('electron-squirrel-startup');
+
+// 在文件顶部添加
+process.on('uncaughtException', (error) => {
+  console.error('未捕获的异常:', error);
+});
+
+process.on('unhandledRejection', (error) => {
+  console.error('未处理的 Promise 拒绝:', error);
+});
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (started) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // 只在开发模式下打开 DevTools
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools();
+  }
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(async () => {
+  createWindow();
+
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+
+const isDev = process.env.NODE_ENV === 'development';
+
+if (isDev) {
+  try {
+    require('electron-reloader')(module);
+  } catch (_) {}
+}
+
+// 在文件最顶部添加
+process.env.ELECTRON_ENABLE_LOGGING = false;
